598b9bf364f305a8c7405ef037e21ce14b4839a3
Adrian <marcelonads@gmail.com>
2024-04-01 09:10:05 +0800

    template: ADDED logger in server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const app = require("./app");
 
 const httpServer = require("http").createServer(app);
 
+const logger = require("./config/winston");
+
 httpServer.listen(process.env.PORT, () => {
-	console.log("Server is running on port: " + process.env.PORT);
+	logger.info("Server is running on port: " + process.env.PORT);
 });
